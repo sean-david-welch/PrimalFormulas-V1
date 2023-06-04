21ac3627387f8f2e7935ae0f0b33ebd7f145151d
Sean Welch <seanwelch51199@icloud.com>
2023-06-04 13:13:29 +0100

    added env modes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,17 +1,14 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-let FRONTEND_BASE_URL: string;
-
-if (import.meta.env.MODE === 'production') {
-    FRONTEND_BASE_URL = import.meta.env.VITE_PRODUCTION_BASE_URL;
-} else {
-    FRONTEND_BASE_URL =
-        import.meta.env.VITE_FRONTEND_BASE_URL ||
-        import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
-}
-
-export { FRONTEND_BASE_URL };
-
 export const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
 
-export const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+let FRONTEND_BASE_URL: string;
+if (import.meta.env.MODE === 'production') {
+    FRONTEND_BASE_URL = import.meta.env.VITE_PRODUCTION_BASE_URL;
+    console.log('Production mode', 'Base URL:', FRONTEND_BASE_URL);
+} else {
+    const FRONTEND_BASE_URL =
+        import.meta.env.VITE_FRONTEND_BASE_URL ||
+        import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
+    console.log('Development mode', 'Base URL:', FRONTEND_BASE_URL);
+}
+export { FRONTEND_BASE_URL };
