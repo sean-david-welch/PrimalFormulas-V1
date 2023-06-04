d5113e2ee767fa96cceda6c44308b7b2b71ae748
Sean Welch <seanwelch51199@icloud.com>
2023-06-04 19:17:48 +0100

    remove console logs

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,24 +5,10 @@ let FRONTEND_BASE_URL: string;
 
 if (import.meta.env.MODE === 'production') {
     FRONTEND_BASE_URL = import.meta.env.VITE_PRODUCTION_BASE_URL;
-    console.log(
-        'Production mode',
-        'Base URL:',
-        FRONTEND_BASE_URL,
-        'API URL:',
-        API_BASE_URL
-    );
 } else {
     FRONTEND_BASE_URL =
         import.meta.env.VITE_FRONTEND_BASE_URL ||
         import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
-    console.log(
-        'Development mode',
-        'Base URL:',
-        FRONTEND_BASE_URL,
-        'API URL:',
-        API_BASE_URL
-    );
 }
 
 export { FRONTEND_BASE_URL };
