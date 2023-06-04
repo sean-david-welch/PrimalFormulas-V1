de587ab18c1c2219783863376829774ecc215a4e
Sean Welch <seanwelch51199@icloud.com>
2023-06-04 13:21:24 +0100

    check modes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,28 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 let FRONTEND_BASE_URL: string;
+
 if (import.meta.env.MODE === 'production') {
     FRONTEND_BASE_URL = import.meta.env.VITE_PRODUCTION_BASE_URL;
-    console.log('Production mode', 'Base URL:', FRONTEND_BASE_URL);
+    console.log(
+        'Production mode',
+        'Base URL:',
+        FRONTEND_BASE_URL,
+        'API URL:',
+        API_BASE_URL
+    );
 } else {
-    const FRONTEND_BASE_URL =
+    FRONTEND_BASE_URL =
         import.meta.env.VITE_FRONTEND_BASE_URL ||
         import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
-    console.log('Development mode', 'Base URL:', FRONTEND_BASE_URL);
+    console.log(
+        'Development mode',
+        'Base URL:',
+        FRONTEND_BASE_URL,
+        'API URL:',
+        API_BASE_URL
+    );
 }
+
 export { FRONTEND_BASE_URL };
