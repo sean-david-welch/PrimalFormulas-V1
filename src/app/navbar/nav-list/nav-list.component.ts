e17e1eb6dfdc193cce6466a2e606c11b293cd644
Sean Welch <seanwelch51199@icloud.com>
2023-08-18 16:51:53 +0100

    successfully implemented some components

--- src/app/navbar/nav-list/nav-list.component.ts
+++ src/app/navbar/nav-list/nav-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User, Supplier } from './nav-list.models';
 
 @Component({
-  selector: 'app-nav-list',
-  templateUrl: './nav-list.component.html',
-  styleUrls: ['./nav-list.component.css']
+    selector: 'app-nav-list',
+    templateUrl: './nav-list.component.html',
+    styleUrls: ['./nav-list.component.css'],
 })
 export class NavListComponent {
-
+    @Input() user?: User | null;
+    @Input() suppliers?: Supplier[];
 }
---
+++ src/app/navbar/nav-list/nav-list.models.ts
@@ -0,0 +1,9 @@
+// nav-list.models.ts
+export interface User {
+    role: string;
+}
+
+export interface Supplier {
+    name: string | null;
+    id: string | null;
+}
