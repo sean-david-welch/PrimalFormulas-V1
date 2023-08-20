94fdd4d706c858db0b8026ac078858bbbf134b6f
Sean Welch <seanwelch51199@icloud.com>
2023-08-20 18:54:10 +0100

    some default styles

--- src/app/components/nav-button/nav-button.component.ts
+++ src/app/components/nav-button/nav-button.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-button',
-  templateUrl: './nav-button.component.html',
-  styleUrls: ['./nav-button.component.css']
+    selector: 'app-nav-button',
+    templateUrl: './nav-button.component.html',
+    styleUrls: ['./nav-button.component.css'],
 })
 export class NavButtonComponent {
-
+    @Input() link: string = '/';
+    @Input() text: string = '';
 }
