5edeb957fefdea2b6355639cd38d222f1d498211
Sean Welch <seanwelch51199@icloud.com>
2023-08-25 14:57:35 +0100

    some component and styling fix

--- src/app/shared/components/layout/navbar/nav-list/nav-logo/nav-logo.component.ts
+++ src/app/shared/components/layout/navbar/nav-list/nav-logo/nav-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-nav-logo',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
     styleUrls: ['./nav-logo.component.css'],
 })
 export class NavLogoComponent {
+    @Input() logoWidth?: number = 112;
+    @Input() logoHeight?: number = 112;
+
     logo = 'https://d3o4nf60ihcs04.cloudfront.net/images/logo.png';
 }
