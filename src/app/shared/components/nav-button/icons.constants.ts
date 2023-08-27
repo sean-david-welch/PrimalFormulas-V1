6e70898d61f511d812354d385d2d4c199d76f3dd
Sean Welch <seanwelch51199@icloud.com>
2023-08-27 15:33:09 +0100

    styled products component && intersection observer

--- src/app/shared/components/nav-button/icons.constants.ts
+++ src/app/shared/components/nav-button/icons.constants.ts
@@ -1,10 +1,12 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import {
     faArrowRight,
+    faCartPlus,
     faRightFromBracket,
 } from '@fortawesome/free-solid-svg-icons';
 
 export const icons: Record<string, IconDefinition> = {
     'arrow-right': faArrowRight,
     'bracket-right': faRightFromBracket,
+    'cart-plus': faCartPlus,
 };
